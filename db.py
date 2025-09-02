c2ed942cc972cfe7d7d77d3c01e459300437efa7
walt <dineshdk26072004@gmail.com>
2025-09-02 12:40:41 +0530

    added postgresql db

--- db.py
+++ db.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-DATABASE_URL = "sqlite:///./students.db"  
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Replace this with your Render PostgreSQL connection string
+# Format: "postgresql://username:password@host:5432/dbname"
+DATABASE_URL = "postgresql://bookapi_db_6ok6_user:g0zCkpD3MnWyiuLaRQNSO5TihbywcNBu@dpg-d2r8tbvdiees73e1ev80-a/bookapi_db_6ok6"
 
+# Create engine (no connect_args needed for PostgreSQL)
+engine = create_engine(DATABASE_URL)
+
+# Session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class
 Base = declarative_base()
