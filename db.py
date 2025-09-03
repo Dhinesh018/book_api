ccff2ceb20bca5d48ae210649e496692944970f4
walt <dineshdk26072004@gmail.com>
2025-09-03 13:38:05 +0530

    updated database url

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Replace this with your Render PostgreSQL connection string
 # Format: "postgresql://username:password@host:5432/dbname"
-DATABASE_URL = "postgresql://bookapi_db_6ok6_user:g0zCkpD3MnWyiuLaRQNSO5TihbywcNBu@dpg-d2r8tbvdiees73e1ev80-a/bookapi_db_6ok6"
+DATABASE_URL = "postgresql://bookapi_db_6ok6_user:g0zCkpD3MnWyiuLaRQNSO5TihbywcNBu@dpg-d2r8tbvdiees73e1ev80-a.oregon-postgres.render.com/bookapi_db_6ok6"
 
 # Create engine (no connect_args needed for PostgreSQL)
 engine = create_engine(DATABASE_URL)
