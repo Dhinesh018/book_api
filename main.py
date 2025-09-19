a542cdbb7dd8095e0def2e9a223bc1e17c0c53aa
walt <dineshdk26072004@gmail.com>
2025-09-19 12:29:01 +0530

    updated main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # fallback to 8000 for local dev
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 
+
 #dependency
 def get_db():
     db = SessionLocal()
